33f0f32a462ec05fe65ec861b4859d8eb64dbd7b
Javier Vivas <oscarvijuarezdeveloper@gmail.com>
2018-04-24 18:52:23 -0500

    Primer commit al proyecto de Angular Avanzado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/*Routes*/
+import { APP_ROUTES } from './app.routes';
 
+/*Components*/
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
+/* Services*/
+import { SharedService } from './services/shared.service';
+import { SidebarService } from './services/sidebar.service';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graphics1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
-  providers: [],
+  providers: [
+    SharedService,
+    SidebarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
